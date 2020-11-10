39a270caf0b3c2d5133d78915ed1f00d75f4ac3d
Hiroaki Koga <h.koga@icloud.com>
2020-11-10 09:07:52 +0900

    React課題①-1修正

--- src/js/components/Birthday.js
+++ src/js/components/Birthday.js
@@ -3,39 +3,13 @@ import React from "react";
 export default class Birthday extends React.Component {
     render(){
       const years = [
-        {value: '-', label: '-'},
-        {value: '1990', label: '1990'},
-        {value: '1991', label: '1991'},
-        {value: '1992', label: '1992'},
-        {value: '1993', label: '1993'},
-        {value: '1994', label: '1994'},
-        {value: '1995', label: '1995'},
-        {value: '1996', label: '1996'},
-        {value: '1997', label: '1997'},
-        {value: '1998', label: '1998'},
-        {value: '1999', label: '1999'},
-        {value: '2000', label: '2000'},
-        {value: '2001', label: '2001'},
-        {value: '2002', label: '2002'},
-        {value: '2003', label: '2003'},
-        {value: '2004', label: '2004'},
-        {value: '2005', label: '2005'},
-        {value: '2006', label: '2006'},
-        {value: '2007', label: '2007'},
-        {value: '2008', label: '2008'},
-        {value: '2009', label: '2009'},
-        {value: '2010', label: '2010'},
-        {value: '2011', label: '2011'},
-        {value: '2012', label: '2012'},
-        {value: '2013', label: '2013'},
-        {value: '2014', label: '2014'},
-        {value: '2015', label: '2015'},
-        {value: '2016', label: '2016'},
-        {value: '2017', label: '2017'},
-        {value: '2018', label: '2018'},
-        {value: '2019', label: '2019'},
-        {value: '2020', label: '2020'},
+        {value: "-", label:'-'}
       ];
+      for(let i=1990; i<2021; i++){
+            years[i-1989].value = i,
+            years[i-1989].label =  i
+          };
+
       const months = [
         {value: '-', label: '-'},
         {value: '1', label: '1'},
@@ -86,9 +60,9 @@ export default class Birthday extends React.Component {
         {value: '31', label: '31'},
       ]
     
-      const selectYear = years.map( (year) => 
-       <option value={year.value}>{year.label}</option>
-      );
+      // const selectYear = years.map( (year) => 
+      //  <option value={year.value}>{year.label}</option>
+      // );
       const selectMonth = months.map((month) => 
         <option value={month.value}>{month.label}</option>
       );
@@ -103,7 +77,7 @@ export default class Birthday extends React.Component {
               <div className="select">
                 <form>
                     <select class="birthday-year" name="year">
-                      {selectYear}
+                      {/* {selectYear} */}
                     </select>　年
                     <select class="birthday-month" name="month">
                       {selectMonth}
