d216cab950013867ec3302239c6c08721de7be7c
Hiroaki Koga <h.koga@icloud.com>
2020-12-16 19:36:03 +0900

    React課題①-2修正

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -11,23 +11,8 @@ const initState = {
 
 const reducer = (state = initState, action) => {
   switch (action.type) {
-    case 'UPD_FIRST_STATUS':
-      return Object.assign({}, state, {
-        sex: action.value.sex,
-        birthYear: action.value.birthYear,
-        birthMonth: action.value.birthMonth,
-        birthDay: action.value.birthDay,
-      });
-    case 'UPD_SECOND_STATUS':
-      return Object.assign({}, state, {
-        insuStatus: action.value.insuStatus,
-        hospiStatus: action.value.hospiStatus,
-        pastStatus: action.value.pastStatus,
-      });
-    case 'UPD_THIRD_STATUS':
-      return Object.assign({}, state, {
-        consultations: action.value.consultations,
-      });
+    case 'UPD_STATUS':
+      return Object.assign(state, action.value);
     default: {
       return state;
     }
